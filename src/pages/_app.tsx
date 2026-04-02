e33901737395622cc5f9dadaa10685f092741672
Yan <yan.fishel@gmail.com>
2026-04-02 05:04:28 +0300

    Add web app manifest and update favicons for improved PWA support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,9 @@ import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '@/src/theme';
 
-
-
 import '@mantine/core/styles.css';
 import '@/src/styles/global.css';
 import '@/src/styles/highlights.css';
-
-
-
 
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -23,7 +18,14 @@ export default function App({ Component, pageProps }: AppProps) {
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <meta name="description" content="Code Frame. Beautiful Code Snippet Image" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Code Frame" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#dee2e6" />
+        <meta name="theme-color" content="#dee2e6" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
